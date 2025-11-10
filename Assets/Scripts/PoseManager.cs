9ce2dae0f6db241ac821cbc620c44ee31bed22e7
DGIST2023choidoyun <cdydoyun@dgist.ac.kr>
2025-11-11 04:18:57 +0900

    hinge limit & layering

--- Assets/Scripts/PoseManager.cs
+++ Assets/Scripts/PoseManager.cs
@@ -3,29 +3,18 @@ using System.Collections.Generic;
 
 public class PoseManager : MonoBehaviour
 {
-    [SerializeField] private Rigidbody2D ragdoll;
+    [SerializeField] private Ragdoll ragdoll;
 
-    private bool activated = false;
 
     private void Awake()
     {
-        UpdateMode();
+        ragdoll.UpdateMode();
     }
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.A))
         {
-            UpdateMode();
-        }
-    }
-
-    private void UpdateMode()
-    {
-        Muscle[] muscles = ragdoll.GetComponentsInChildren<Muscle>();
-        activated = !activated;
-        foreach (Muscle muscle in muscles)
-        {
-            muscle.musclesActive = !activated;
+            ragdoll.UpdateMode();
         }
     }
 }
--- Assets/Scripts/RagdollSet.cs
+++ Assets/Scripts/RagdollSet.cs
@@ -8,19 +8,8 @@ public class RagdollSet : MonoBehaviour
 
     void Awake()
     {
-        List<Collider2D> allColliders = new List<Collider2D>();
-        // 1. 현재 객체와 모든 자식 객체에서 Collider 2D를 찾습니다.
-        Collider2D[] colliders = GetComponentsInChildren<Collider2D>();
-        List<Muscle> loadCalcPool = new();
         HashSet<Muscle> finishCalcPool = new();
-        
-        // 2. 리스트에 모든 Collider를 추가합니다.
-        allColliders.AddRange(colliders);
 
-        // 3. 리스트에 있는 모든 Collider 쌍을 서로 충돌 무시 설정합니다.
-        IgnoreSelfAndChildrenCollisions(allColliders);
-
-        // 4. 모든 파츠의 하중 계산을 시작합니다 (Post-order Traversal 적용).
         foreach (Muscle keyBase in lastBases)
         {
             // 각 루트 파츠(keyBase)로부터 재귀적으로 하향 탐색을 시작합니다.
@@ -58,26 +47,5 @@ public class RagdollSet : MonoBehaviour
         // 3. 계산 완료 풀에 추가
         calculatedParts.Add(currentPart);
         
-    }
-
-    private void IgnoreSelfAndChildrenCollisions(List<Collider2D> allColliders)
-    {
-        // 리스트의 각 Collider(colliderA)를 선택합니다.
-        for (int i = 0; i < allColliders.Count; i++)
-        {
-            Collider2D colliderA = allColliders[i];
-
-            // colliderA 이후의 모든 Collider(colliderB)와 쌍을 이룹니다.
-            // (이미 이전 반복에서 처리한 쌍은 건너뛰어 중복 설정을 방지합니다.)
-            for (int j = i + 1; j < allColliders.Count; j++)
-            {
-                Collider2D colliderB = allColliders[j];
-
-                // Physics2D.IgnoreCollision 메서드를 사용하여 두 Collider가 충돌을 무시하도록 설정합니다.
-                Physics2D.IgnoreCollision(colliderA, colliderB, true);
-                
-                // Debug.Log($"Ignored collision between: {colliderA.name} and {colliderB.name}");
-            }
-        }
     }
 }
